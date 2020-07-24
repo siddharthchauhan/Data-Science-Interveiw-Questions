03b129b93a670b1a063dd061590e47bf77a2d995
Siddharth <siddharthchauhan@users.noreply.github.com>
2020-07-24 13:45:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,32 @@
 import pandas as pd
 import streamlit as st
 
+question = ["Data Scientist", "Data Analyst"]
+choice = st.sidebar.selectbox("Select your field", question)
 
-def main():
+if choice == 'Data Scientist':
 
-    question = ["Data Scientist", "Data Analyst"]
-    choice = st.sidebar.selectbox("Select your field", question)
+    df_ds = pd.read_excel('Glassdoor Data Scientist Interview Questions.xlsx')
 
-    if choice == 'Data Scientist':
+    df_ds['Date'] = pd.to_datetime(df_ds['Date'])
+    df_ds['Year']= df_ds['Date'].dt.year
 
-            df = pd.read_excel('Glassdoor Data Scientist Interview Questions.xlsx')
+    company_title = st.sidebar.selectbox('Company Name', sorted(df_ds['Company'].unique()))
+    date_asked = st.sidebar.selectbox('Year', df_ds['Year'].unique())
+    st.title(company_title)
+    st.subheader(date_asked)
+    st.table(df_ds[(df_ds["Company"] == company_title) & (df_ds["Year"] == date_asked)])
 
-            company = sorted(df['Company'].unique())
-            df['Date'] = pd.to_datetime(df['Date'])
-            df['Year']= df['Date'].dt.year
-            year = df['Year'].unique()
+elif choice == 'Data Analyst':
 
-            if not st.sidebar.checkbox("Close", True):
-                company_title = st.sidebar.selectbox('Company Name', company)
-                st.title(company_title)
 
-                filter_question = (df['Company'] == company_title)
-                st.table(df[filter_question])
+    df = pd.read_excel('Glassdoor Data Analyst Interview Questions.xlsx')
 
-            if not st.sidebar.checkbox("Hide", True):
-                date_asked = st.sidebar.selectbox('Year', year)
-                st.title(date_asked)
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['Year']= df['Date'].dt.year
 
-                fil_question = (df['Year'] == date_asked)
-                st.table(df[fil_question])
-
-    elif choice == 'Data Analyst':
-
-            df = pd.read_excel('Glassdoor Data Analyst Interview Questions.xlsx')
-            com = sorted(df['Company'].unique())
-            df['Date'] = pd.to_datetime(df['Date'])
-            df['Year'] = df['Date'].dt.year
-            year = df['Year'].unique()
-
-            if not st.sidebar.checkbox("Close", True):
-                comp_title = st.sidebar.selectbox('Company Name', com)
-                st.title(comp_title)
-
-                filt_question = (df['Company'] == comp_title)
-                st.table(df[filt_question])
-
-            if not st.sidebar.checkbox("Hide", True):
-                date_ask = st.sidebar.selectbox('Year', year)
-                st.title(date_ask)
-
-                fil_ques = (df['Year'] == date_ask)
-                st.table(df[fil_ques])
-
-if __name__ == '__main__':
-    main()
+    company_title = st.sidebar.selectbox('Company Name', sorted(df['Company'].unique()))
+    date_asked = st.sidebar.selectbox('Year', sorted(df['Year'].unique()))
+    st.title(company_title)
+    st.subheader(date_asked)
+    st.table(df[(df["Company"] == company_title) & (df["Year"] == date_asked)])
